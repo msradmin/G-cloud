b92adf865f7f467e9a7784bd537a8eeb350e7778
msradmin <86022985+msradmin@users.noreply.github.com>
2022-08-08 07:08:58 -0700

    Update Win7VNC.sh

--- Win7VNC.sh
+++ Win7VNC.sh
@@ -8,19 +8,18 @@ sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 clear
-echo Downloading Windows files from aank.me
+echo Installing Windows
 rm -rf win7 win7.img
 curl -L -o win7.gz https://ia902208.us.archive.org/35/items/w7-qcow2/win7lite.img.gz
 echo "Wait.."
 gunzip win7.gz
 mv win7 win7.img
-echo "Windows 7 x86 Lite On Gitpod, GCloud and GColab"
+echo "Windows 7 x86 Lite On Goorm.IDE"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Don't Close this Tab"
-echo "Support YT Channel-> Aank is ME"
-echo "LinK: https://aank.me/Youtube"
+echo "Thanks for installing!"
 b='\033[1m'
 r='\E[31m'
 g='\E[32m'
@@ -40,5 +39,5 @@ printf """$c$b
     $r  Support YT Channel-> Aank is ME © 2022 $c https://aank.me/Youtube 
           
 $endc$enda""";
-sudo qemu-system-x86_64 -vnc :0 -hda win7.img  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda win7.img -m 800 -machine usb=on -device usb-tablet > /dev/null 2>&1
 sleep 43200
